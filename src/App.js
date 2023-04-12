0c6fccc083b8b39830cf2b0d61a5fe164425a331
Erjik1 <120021425+Erjik1@users.noreply.github.com>
2023-04-12 19:52:00 +0600

    dz8-erjan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const RegisterForm = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-export default App;
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="name">ФИО:</label>
+                <input id="name" {...register("name", { required: true })} />
+                {errors.name && <span>Это поле обязательно</span>}
+            </div>
+            <div>
+                <label htmlFor="firstName">Имя:</label>
+                <input id="firstName" {...register("firstName", { required: true })} />
+                {errors.firstName && <span>Это поле обязательно</span>}
+            </div>
+            <div>
+                <label htmlFor="birthYear">Год рождения:</label>
+                <select id="birthYear" {...register("birthYear", { required: true })}>
+                    <option value="">Выберите год</option>
+                    {Array.from({ length: 100 }, (_, i) => new Date().getFullYear() - i).map((year) => (
+                        <option key={year} value={year}>{year}</option>
+                    ))}
+                </select>
+                {errors.birthYear && <span>Это поле обязательно</span>}
+            </div>
+            <div>
+                <label htmlFor="gender">Пол:</label>
+                <select id="gender" {...register("gender", { required: true })}>
+                    <option value="">Выберите пол</option>
+                    <option value="male">Мужской</option>
+                    <option value="female">Женский</option>
+                    <option value="other">Другой</option>
+                </select>
+                {errors.gender && <span>Это поле обязательно</span>}
+            </div>
+            <button type="submit">Зарегистрироваться</button>
+        </form>
+    );
+};
+
+export default RegisterForm;
